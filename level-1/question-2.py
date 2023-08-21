c1a848d5be0b20a6e16ae8e82f7d7663f3ce571f
Chris van den Berg <cvandenberg1105@googlemail.com>
2023-08-22 00:34:00 +0100

    Create IntegerFactorial class and methods for calculating question task

---
+++ level-1/question-2.py
@@ -0,0 +1,25 @@
+class IntegerFactorial:
+
+    def __init__(self, integer_list: list[int]):
+        self.integer_list = integer_list
+
+    def factorial(self, integer: int) -> int:
+        if (integer == 0) or (integer == 1):
+            return 1
+
+        return integer * self.factorial(integer - 1)
+
+    def calculate_factorial(self) -> dict[int, int]:
+        factorial_list: dict[int, int] = {}
+        for number in self.integer_list:
+            factorial_list[number] = self.factorial(number)
+        return factorial_list
+
+
+factorial_integer_list: list[int] = []
+for integer in range(1, 10):
+    factorial_integer_list.append(integer)
+
+integer_factorial: IntegerFactorial = IntegerFactorial(factorial_integer_list)
+factorials: dict[int, int] = integer_factorial.calculate_factorial()
+print(factorials)
