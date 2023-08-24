ee5f702fb13c8989f6206d2982ff31c844d43b83
Chris van den Berg <cvandenberg1105@googlemail.com>
2023-08-24 22:55:58 +0100

    Finish question class logic and create custom DataStructureSize exception

---
+++ helpers/exception_helpers.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+class DataStructureSizeError(Exception):
+    def __init__(self, expected_size: int, actual_size: int):
+        self.expected_size = expected_size
+        self.actual_size = actual_size
+        super().__init__(f"Expected size {expected_size}, but got size {actual_size}")
--- level_two/question_seven.py
+++ level_two/question_seven.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from helpers import string_helpers
+from helpers import string_helpers, exception_helpers
 
 
 class TwoDimensionalArray:
@@ -9,6 +9,33 @@ class TwoDimensionalArray:
         self.string_list_helper = string_list_helper
 
     def _get_user_input(self) -> list[str]:
-        user_input: str = input('Please enter a comma separated sequence of numbers:')
-        return self.string_list_helper.get_validated_string_list(user_input, ',')
+        user_input: str = input('Please enter a comma separated sequence of two numbers:')
+        user_input_list: list[str] = self.string_list_helper.get_validated_string_list(user_input, ',')
+
+        if len(user_input_list) != 2:
+            raise exception_helpers.DataStructureSizeError(2, len(user_input_list))
+
+        return user_input_list
+
+    def calculate_two_dimensional_array(self) -> list[list[int]]:
+        user_input_list = self._get_user_input()
+        two_dimensional_array: list[list[int]] = []
+
+        outer_list_size: int = round(float(user_input_list[0]))
+        inner_list_size: int = round(float(user_input_list[1]))
+
+        for outer_element in range(outer_list_size):
+            row: list[int] = []
+            for inner_element in range(inner_list_size):
+                row.append(outer_element * inner_element)
+            two_dimensional_array.append(row)
+
+        return two_dimensional_array
+
+
+string_list_helper_obj: string_helpers.StringListHelper() = string_helpers.StringListHelper()
+two_dimensional_array_obj = TwoDimensionalArray(string_list_helper_obj)
+
+print(two_dimensional_array_obj.calculate_two_dimensional_array())
+
 
