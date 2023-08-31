bda06f517a4c27fd15e5bcf79d4a7098fb5542f2
Chris van den Berg <cvandenberg1105@googlemail.com>
2023-08-31 22:29:55 +0100

    Question 31 main.py

--- level_3/question_31/__main__.py
+++ level_3/question_31/__main__.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
+from shape_class_subclass import Square, Rectangle, Circle
+
 
 def main():
+    square: Square = Square(5.00)
+    print(square.calculate_area())
 
+    rectangle: Rectangle = Rectangle(10.00, 5.00)
+    print(rectangle.calculate_area())
+
+    circle: Circle = Circle(2.50)
+    print(format(circle.calculate_area(), ".2f"))
 
 
 if __name__ == '__main__':
