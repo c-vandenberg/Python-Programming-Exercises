c285db9e6d7f5736ab0f75d903d6ba1c76d1beb6
Chris van den Berg <cvandenberg1105@googlemail.com>
2023-09-18 13:54:29 +0100

    heapify doesn't return new list with heap structure, it simple sorts the list into heap structure

--- data_structures/exercise_48/__main__.py
+++ data_structures/exercise_48/__main__.py
@@ -5,19 +5,19 @@ from heaps import MinHeapq, MaxHeapq
 
 
 def main():
-    min_heap_list = [integer for integer in range(0, 101)]
+    min_heap = [integer for integer in range(0, 101)]
 
     min_heapq: MinHeapq = MinHeapq()
-    min_heap: List[int] = min_heapq.heapify_list(min_heap_list)
+    min_heap: List[int] = min_heapq.heapify_list(min_heap)
     min_root_element: int = min_heapq.pop(min_heap)
     min_heapq.push(min_heap, 110)
     min_heapq.push_pop(min_heap, 102)
     min_heapq.replace_root(min_heap, 1)
 
-    max_heap_list = [integer for integer in range(0, 101)]
+    max_heap = [integer for integer in range(0, 101)]
 
     max_heapq: MaxHeapq = MaxHeapq()
-    max_heap: List[int] = max_heapq.heapify_list(max_heap_list)
+    max_heapq.heapify_list(max_heap)
     max_root_element: int = max_heapq.pop(max_heap)
     max_heapq.push(max_heap, 110)
     max_heapq.push_pop(max_heap, 102)
